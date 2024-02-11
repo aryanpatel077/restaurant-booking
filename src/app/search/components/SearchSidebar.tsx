a3357bd800003e032ddace50d15a9b843ef99c06
aryanpatel7987 <aryanpatel7987@gmail.com                                    $ git config --global user.name Aryan>
2024-02-11 14:09:00 +0530

    search parameter completed

--- src/app/search/components/SearchSidebar.tsx
+++ src/app/search/components/SearchSidebar.tsx
@@ -1,33 +1,83 @@
-import { Location, Region } from "@prisma/client";
+import { Location, PRICE, Region } from "@prisma/client";
+import Link from "next/link";
 import React from "react";
 
-export default function SearchSidebar({locations,regions}: {locations: Location[], regions: Region[]}) {
+export default function SearchSidebar({
+  locations,
+  regions,
+  searchParams,
+}: {
+  locations: Location[];
+  regions: Region[];
+  searchParams: { city?: string; region?: string; price?: PRICE };
+}) {
+
+  const price =[
+    {
+      price: PRICE.CHEAP,
+      label: "$",
+      className: "border w-full text-reg text-center font-light rounded-l p-2"
+    },
+    {
+      price: PRICE.REGULAR,
+      label: "$$",
+      className: "border-r border-t border-b w-full text-reg text-center font-light p-2"
+    },
+    {
+      price: PRICE.EXPENSIVE,
+      label: "$$$",
+      className: "border-r border-t border-b w-full text-reg text-center font-light p-2 rounded-r"
+    }
+  ]
+
   return (
     <div className="w-1/5">
       <div className="border-b pb-4">
         <h1 className="mb-2">Location</h1>
-        {locations.map((location: Location) => (
-           <p className="font-light text-reg capitalize">{location.name}</p>
-        ))}  
+        <div className="flex flex-col">
+          {locations.map((location: Location) => (
+            <Link
+              href={{
+                pathname: "/search",
+                query: { ...searchParams, city: location.name },
+              }}
+              className="font-light text-reg capitalize"
+            >
+              {location.name}
+            </Link>
+          ))}
+        </div>
       </div>
       <div className="border-b pb-4 mt-3">
         <h1 className="mb-2">Region</h1>
-        {locations.map((region: Region) => (
-           <p className="font-light text-reg capitalize">{region.name}</p>
-           ))}  
+        <div className="flex flex-col">
+          {regions.map((region: Region) => (
+            <Link
+              href={{
+                pathname: "/search",
+                query: {  ...searchParams, region: region.name },
+              }}
+              className="font-light text-reg capitalize"
+            >
+              {region.name}
+            </Link>
+          ))}
+        </div>
       </div>
       <div className="mt-3 pb-4">
         <h1 className="mb-2">Price</h1>
         <div className="flex">
-          <button className="border w-full text-reg font-light rounded-l p-2">
-            $
-          </button>
-          <button className="border-r border-t border-b w-full text-reg font-light p-2">
-            $$
-          </button>
-          <button className="border-r border-t border-b w-full text-reg font-light p-2 rounded-r">
-            $$$
-          </button>
+          {
+            price.map(({price,label,className}) =>(
+              <Link href={{
+                pathname: "/search",
+                query: { ...searchParams, price},
+              } }
+              className={className} >
+               {label}
+            </Link>
+            ))
+          }
         </div>
       </div>
     </div>
