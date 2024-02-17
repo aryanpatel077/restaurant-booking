5656b3e9c5eedeeeea53bab49a471ab7f8a3ddd9
aryanpatel7987 <aryanpatel7987@gmail.com                                    $ git config --global user.name Aryan>
2024-02-17 14:45:52 +0530

    missed

--- src/hooks/useAuthContext.ts
+++ src/hooks/useAuthContext.ts
@@ -2,9 +2,9 @@ import { AuthenticationContext } from "@/app/context/AuthContext";
 import { useContext } from "react";
 
 export default function useAuthContext() {
-  const { data, error, loading, setAuthState } = useContext(
+  const { data, error, loading,isFetchingUser, setAuthState } = useContext(
     AuthenticationContext
   );
 
-  return { data, error, loading, setAuthState };
+  return { data, error, loading,isFetchingUser, setAuthState };
 }
