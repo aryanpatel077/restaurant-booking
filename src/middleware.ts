a305fc34b090f9b78fb66141a2f82676b86f51e5
aryanpatel077 <aryanpatel7987@gmail.com>
2024-02-25 09:54:20 +0530

    removed some files

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,37 @@
-import { NextRequest, NextResponse } from "next/server"
-import * as jose from "jose"
+// import { NextRequest, NextResponse } from "next/server"
+// import * as jose from "jose"
 
-export default async function middleware(request: NextRequest) {
-  const BearerToken = request.headers.get("authorization") as string
+// export default async function middleware(request: NextRequest) {
+//   const BearerToken = request.headers.get("authorization") as string
 
-  if(!BearerToken){
-    return new NextResponse(JSON.stringify({
-      errorMessage: "Bearer token not defined"
-    }), {status: 401})
-  }
+//   if(!BearerToken){
+//     return new NextResponse(JSON.stringify({
+//       errorMessage: "Bearer token not defined"
+//     }), {status: 401})
+//   }
 
-  const token = BearerToken.split(" ")[1]
+//   const token = BearerToken.split(" ")[1]
 
-  if(!token){
-    return new NextResponse(JSON.stringify({
-      errorMessage: "token not defined"
-    }), {status: 401})
-  }
+//   if(!token){
+//     return new NextResponse(JSON.stringify({
+//       errorMessage: "token not defined"
+//     }), {status: 401})
+//   }
 
-  const signature = new TextEncoder().encode(process.env.JWT_SECRET);
+//   const signature = new TextEncoder().encode(process.env.JWT_SECRET);
 
-  try {
-    await jose.jwtVerify(token, signature)
-  } catch (error) {
-    return new NextResponse(JSON.stringify({
-      errorMessage: "Unauthorized request !! "
-    }), {status: 401})
-  }
-}
+//   try {
+//     await jose.jwtVerify(token, signature)
+//   } catch (error) {
+//     return new NextResponse(JSON.stringify({
+//       errorMessage: "Unauthorized request !! "
+//     }), {status: 401})
+//   }
+// }
 
-export const config = {
-  matcher: ['/api/auth/me']
-}
+// export const config = {
+//   matcher: ['/api/auth/me']
+// }
 
 
 
