5f65b4ba38b9dab5d0bfa3b84b767a4f3895d738
aryanpatel077 <aryanpatel7987@gmail.com>
2024-02-25 09:50:37 +0530

    me page removed

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -1,37 +1,37 @@
-import { NextResponse } from "next/server";
-import { headers } from "next/headers";
-import jwt from "jsonwebtoken";
-import { PrismaClient } from "@prisma/client";
+// import { NextResponse } from "next/server";
+// import { headers } from "next/headers";
+// import jwt from "jsonwebtoken";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export async function GET(request: Request) {
-  const headersInstance = headers();
-  const BearerToken = headersInstance.get("authorization") as string;
-  const token = BearerToken.split(" ")[1];
+// export async function GET(request: Request) {
+//   const headersInstance = headers();
+//   const BearerToken = headersInstance.get("authorization") as string;
+//   const token = BearerToken.split(" ")[1];
 
-  const payload = jwt.decode(token) as { email: string };
+//   const payload = jwt.decode(token) as { email: string };
 
-  const user = await prisma.user.findUnique({
-    where: {
-      email: payload.email,
-    },
-    select: {
-      id: true,
-      first_name: true,
-      last_name: true,
-      email: true,
-      city: true,
-      phone: true,
-    },
-  });
+//   const user = await prisma.user.findUnique({
+//     where: {
+//       email: payload.email,
+//     },
+//     select: {
+//       id: true,
+//       first_name: true,
+//       last_name: true,
+//       email: true,
+//       city: true,
+//       phone: true,
+//     },
+//   });
 
-  return NextResponse.json({
-    id: user?.id,
-    firstName: user?.first_name,
-    lastName: user?.last_name,
-    city: user?.city,
-    phoneNumber: user?.phone,
-    email: user?.email,
-  });
-}
+//   return NextResponse.json({
+//     id: user?.id,
+//     firstName: user?.first_name,
+//     lastName: user?.last_name,
+//     city: user?.city,
+//     phoneNumber: user?.phone,
+//     email: user?.email,
+//   });
+// }
